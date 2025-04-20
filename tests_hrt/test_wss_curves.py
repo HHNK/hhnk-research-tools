79e2e489f6b6a74c0bf8c81a0d22a9dd6bdef855
wvangerwen <41104904+wvangerwen@users.noreply.github.com>
2025-04-20 13:48:13 +0200

    use pd.testing.assert_frame_equal

--- tests_hrt/test_wss_curves.py
+++ tests_hrt/test_wss_curves.py
@@ -1,3 +1,4 @@
+# %%
 # -*- coding: utf-8 -*-
 """
 Created on Thu Sep 26 15:44:05 2024
@@ -39,65 +40,67 @@ RESULT = WSS_DATA / "result.csv"
 RESULT_AGGREGATE = WSS_DATA / "result_aggregate.csv"
 LANDUSE_CONVERSION_TABLE = WSS_DATA / "landuse_conversion_table.csv"
 
-# class TestWSSAggregationFolders:
-#     """Generieke test, in onderstaande test worden ook output bestande getest"""
 
-#     def test_generating_folders(self):
-#         AreaDamageCurveFolders(TEMP_DIR)
+class TestWSSAggregationFolders:
+    """Generieke test, in onderstaande test worden ook output bestande getest"""
 
-#         assert (TEMP_DIR / "work").exists()
-#         assert (TEMP_DIR / "post_processing").exists()
-#         assert (TEMP_DIR / "input").exists()
-#         assert (TEMP_DIR / "output").exists()
+    def test_generating_folders(self):
+        AreaDamageCurveFolders(TEMP_DIR)
+
+        assert (TEMP_DIR / "work").exists()
+        assert (TEMP_DIR / "post_processing").exists()
+        assert (TEMP_DIR / "input").exists()
+        assert (TEMP_DIR / "output").exists()
 
 
-# class TestWSSCurves:
-#     @pytest.fixture(scope="class")
-#     def schadecurves(self):
-#         schadecurves = AreaDamageCurves(
-#             AREA_PATH,
-#             LU_PATH,
-#             DEM_PATH,
-#             OUTPUT_PATH,
-#             area_id=AREA_ID,
-#             curve_max=CURVE_MAX,
-#             curve_step=CURVE_STEP,
-#             area_start_level_field=AREA_START_LEVEL,
-#         )
+class TestWSSCurves:
+    @pytest.fixture(scope="class")
+    def schadecurves(self):
+        schadecurves = AreaDamageCurves(
+            AREA_PATH,
+            LU_PATH,
+            DEM_PATH,
+            OUTPUT_PATH,
+            area_id=AREA_ID,
+            curve_max=CURVE_MAX,
+            curve_step=CURVE_STEP,
+            area_start_level_field=AREA_START_LEVEL,
+        )
 
-#         schadecurves.wss_config_file = WSS_CFG_FILE
-#         schadecurves.wss_settings_file = WSS_SETTINGS_FILE
-#         schadecurves.wss_curves_filter_settings_file = WSS_CURVE_FILTER_SETTINGS_FILE
+        schadecurves.wss_config_file = WSS_CFG_FILE
+        schadecurves.wss_settings_file = WSS_SETTINGS_FILE
+        schadecurves.wss_curves_filter_settings_file = WSS_CURVE_FILTER_SETTINGS_FILE
 
-#         return schadecurves
+        return schadecurves
 
-#     @pytest.fixture(scope="class")
-#     def output(self):
-#         output = pd.read_csv(RESULT)
-#         return output
+    @pytest.fixture(scope="class")
+    def output(self):
+        output = pd.read_csv(RESULT)
+        return output
 
-#     def test_integrated_1d_mp(self, schadecurves, output):
-#         # shutil.rmtree(OUTPUT_PATH)
-#         schadecurves.run(run_1d=True, multiprocessing=True, processes=4)
-#         test_output = pd.read_csv(schadecurves.dir.output.result.path)
-#         assert (output == test_output).all()[0]
+    # def test_integrated_1d_mp(self, schadecurves, output):
+    #     # FIXME SLOW
+    #     # shutil.rmtree(OUTPUT_PATH)
+    #     schadecurves.run(run_1d=True, multiprocessing=True, processes=4)
+    #     test_output = pd.read_csv(schadecurves.dir.output.result.path)
+    #     pd.testing.assert_frame_equal(output, test_output)
 
-#     def test_integrated_1d(self, schadecurves, output):
-#         schadecurves.run(run_1d=True, multiprocessing=False)
-#         test_output = pd.read_csv(schadecurves.dir.output.result.path)
-#         assert (output == test_output).all()[0]
+    def test_integrated_1d(self, schadecurves, output):
+        schadecurves.run(run_1d=True, multiprocessing=False)
+        test_output = pd.read_csv(schadecurves.dir.output.result.path)
+        pd.testing.assert_frame_equal(output, test_output)
 
-#     def test_integrated_2d_mp(self, schadecurves, output):
-#         schadecurves.run(run_2d=True, multiprocessing=True, processes=4, nodamage_filter=True)
+    def test_integrated_2d_mp(self, schadecurves, output):
+        schadecurves.run(run_2d=True, multiprocessing=True, processes=4, nodamage_filter=True)
 
-#         test_output = pd.read_csv(schadecurves.dir.output.result.path)
-#         assert (output == test_output).all()[0]
+        test_output = pd.read_csv(schadecurves.dir.output.result.path)
+        pd.testing.assert_frame_equal(output, test_output)
 
-#     def test_integrated_2d(self, schadecurves, output):
-#         schadecurves.run(run_2d=True, multiprocessing=False, processes=4, nodamage_filter=True)
+    def test_integrated_2d(self, schadecurves, output):
+        schadecurves.run(run_2d=True, multiprocessing=False, processes=4, nodamage_filter=True)
 
-#         test_output = pd.read_csv(schadecurves.dir.output.result.path)
-#         assert (output == test_output).all()[0]
+        test_output = pd.read_csv(schadecurves.dir.output.result.path)
+        pd.testing.assert_frame_equal(output, test_output)
 
 
 class TestWSSAggregation:
@@ -121,4 +124,17 @@ class TestWSSAggregation:
     def test_agg_methods(self, aggregatie, output):
         aggregatie.run()
         test_output = pd.read_csv(aggregatie.dir.post_processing["Wieringermeer"].aggregate.path)
-        assert (test_output == output).all()[0]
+        pd.testing.assert_frame_equal(output, test_output)
+
+
+# %%
+if __name__ == "__main__":
+    selftest = TestWSSCurves()
+    output = selftest.schadecurves()
+    output = selftest.output()
+
+    selftest = TestWSSAggregation()
+    aggregatie = selftest.aggregatie()
+    output = selftest.output()
+
+    selftest.test_agg_methods(aggregatie=aggregatie, output=output)
