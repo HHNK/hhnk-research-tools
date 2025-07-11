8483592cbc3d1254039aabe0b426978f3374d21d
wvangerwen <41104904+wvangerwen@users.noreply.github.com>
2025-07-11 13:53:11 +0200

    add line_geometries_to_coords to init so htt keeps working

--- hhnk_research_tools/threedi/__init__.py
+++ hhnk_research_tools/threedi/__init__.py
@@ -1,10 +1,3 @@
 import lazy_loader as _lazy
 
 __getattr__, __dir__, __all__ = _lazy.attach_stub(__name__, __file__)
-
-# TODO old, remove when all works.
-# from hhnk_research_tools.threedi.geometry_functions import (
-#     coordinates_to_points,
-#     grid_nodes_to_gdf,
-#     line_geometries_to_coords,
-# )
--- hhnk_research_tools/threedi/__init__.pyi
+++ hhnk_research_tools/threedi/__init__.pyi
@@ -1,10 +1,11 @@
 __all__ = [
-    "call_api",
-    "geometry_functions",
-    "grid",
-    "read_api_file",
-    "threediresult_loader",
-    "variables",
+    "variables",  # dir
+    "call_api",  # file
+    "geometry_functions",  # file
+    "grid",  # file
+    "read_api_file",  # file
+    "threediresult_loader",  # file
+    "line_geometries_to_coords",  # func
 ]
 
 from . import (
@@ -14,4 +15,7 @@ from . import (
     read_api_file,
     threediresult_loader,
     variables,
+)
+from .geometry_functions import (
+    line_geometries_to_coords,
 )
