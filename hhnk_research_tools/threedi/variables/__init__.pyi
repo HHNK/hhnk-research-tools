572b6de354c674371f17ea876c16325f4c3ae619
Wietse van Gerwen <41104904+wvangerwen@users.noreply.github.com>
2025-07-15 17:17:37 +0200

    Add lazy loader (#87)
    * lazy loading
    
    * logger.py to logging.py
    
    * add ERROR as loglevel option
    
    * add inits to subdirs
    
    * disable timers
    
    * move rasterio installation check to raster_class
    
    * typechecking issues
    
    moved to hatchling, following https://github.com/mne-tools/mne-python/issues/12169
    still not all working, typechecking is still iffy.
    
    * add __all__ to .pyi for mypy to work.
    
    Follow https://github.com/scikit-image/scikit-image/blob/main/skimage/__init__.pyi example
    
    * fix gh tests
    
    * remove timer prints
    
    * add variables to inits
    
    * add line_geometries_to_coords to init so htt keeps working

---
+++ hhnk_research_tools/threedi/variables/__init__.pyi
@@ -0,0 +1,13 @@
+__all__ = [
+    "gridadmin",
+    "rain_dataframe",
+    "results_mapping",
+    "variables_container",
+]
+
+from . import (
+    gridadmin,
+    rain_dataframe,
+    results_mapping,
+    variables_container,
+)
