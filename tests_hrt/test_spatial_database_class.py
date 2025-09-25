376f2be1858ec8c147ada7b81ed7462e4ed0369a
Wietse van Gerwen <41104904+wvangerwen@users.noreply.github.com>
2025-09-25 12:23:32 +0200

    rename test vars and remove deprecation warning

--- tests_hrt/test_spatial_database_class.py
+++ tests_hrt/test_spatial_database_class.py
@@ -1,5 +1,5 @@
 # %%
-import os
+import shutil
 
 import geopandas as gpd
 import pandas as pd
@@ -9,39 +9,41 @@ from tests_hrt.config import TEMP_DIR, TEST_DIRECTORY
 
 
 def test_spatial_database_class():
-    # load database
-    model_fp = TEST_DIRECTORY / "mini_test_model.gpkg"
-    spdb = SpatialDatabase(model_fp)
+    # Load database
+    model_filepath = TEST_DIRECTORY / "mini_test_model.gpkg"
+    spatial_db = SpatialDatabase(base=model_filepath)
 
-    # test available layers
-    layers = spdb.available_layers()
+    # Test available layers
+    layers = spatial_db.available_layers()
     assert len(layers) == 3
     assert "channel" in layers
 
-    # test loading layer with geometry
-    gdf = spdb.load(layer="channel", index_column="id")
+    # Test loading layer with geometry
+    gdf = spatial_db.load(layer="channel", index_column="id")
     assert isinstance(gdf, gpd.GeoDataFrame)
     assert gdf.crs.to_epsg() == 28992
-    # assert "id" in gdf.index.names
+    assert "id" in gdf.index.names
     assert gdf["code"][514] == "29_1"
 
-    # test loading layer without geometry
-    df = spdb.load(layer="model_settings", columns=["dem_file", "friction_coefficient"])  # non-spatial table
+    # Test loading layer without geometry
+    df = spatial_db.load(layer="model_settings", columns=["dem_file", "friction_coefficient"])  # non-spatial table
     assert isinstance(df, pd.DataFrame)
     assert len(df.columns) == 2
     assert df["dem_file"][0] == "dem_hoekje.tif"
     assert df["friction_coefficient"][0] == 666
 
-    # test modifying using query
-    # copy to temp dir
-    temp_fp = TEMP_DIR / "temp_test_model.gpkg"
-    os.system(f"cp {model_fp} {temp_fp}")
-    spdb = SpatialDatabase(temp_fp)
-    # modify friction coefficient
+    # Test modifying using query
+    temp_filepath = TEMP_DIR / "temp_test_model.gpkg"
+    shutil.copy(src=model_filepath, dst=temp_filepath)
+    spatial_db = SpatialDatabase(temp_filepath)
+
+    # Modify friction coefficient
     query = "UPDATE model_settings SET friction_coefficient = 999"
-    spdb.modify_gpkg_using_query(query=query)
-    df_mod = spdb.load(layer="model_settings", columns=["dem_file", "friction_coefficient"])  # non-spatial table
-    assert df_mod["friction_coefficient"][0] == 999
+    spatial_db.modify_gpkg_using_query(query=query)
+    df_modified = spatial_db.load(
+        layer="model_settings", columns=["dem_file", "friction_coefficient"]
+    )  # non-spatial table
+    assert df_modified["friction_coefficient"][0] == 999
 
 
 # %%
