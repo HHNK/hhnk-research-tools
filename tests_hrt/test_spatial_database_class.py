d88758a5bbb5e48d9ec42f86f1d1536a64c39098
Wouter van Esse <48908991+wvanesse@users.noreply.github.com>
2025-09-25 09:07:12 +0200

    added temp file for modify test

--- tests_hrt/test_spatial_database_class.py
+++ tests_hrt/test_spatial_database_class.py
@@ -1,14 +1,11 @@
 # %%
+import os
+
 import geopandas as gpd
 import pandas as pd
 
 from hhnk_research_tools.folder_file_classes.spatial_database_class import SpatialDatabase
-from tests_hrt.config import TEST_DIRECTORY
-
-
-def test_available_layers():
-    model_fp = TEST_DIRECTORY / "mini_test_model.gpkg"
-    spdb = SpatialDatabase(model_fp)
+from tests_hrt.config import TEMP_DIR, TEST_DIRECTORY
 
 
 def test_spatial_database_class():
@@ -36,15 +33,15 @@ def test_spatial_database_class():
     assert df["friction_coefficient"][0] == 666
 
     # test modifying using query
+    # copy to temp dir
+    temp_fp = TEMP_DIR / "temp_test_model.gpkg"
+    os.system(f"cp {model_fp} {temp_fp}")
+    spdb = SpatialDatabase(temp_fp)
+    # modigy friction coefficient
     query = "UPDATE model_settings SET friction_coefficient = 999"
     spdb.modify_gpkg_using_query(query=query)
     df_mod = spdb.load(layer="model_settings", columns=["dem_file", "friction_coefficient"])  # non-spatial table
     assert df_mod["friction_coefficient"][0] == 999
-    # test predifine layers
-    new_layers = ["aap", "noot", "mies"]
-    spdb.add_layers(new_layers)
-
-    layers = spdb.available_layers()  # TODO doesn't list new layers, how to fix?
 
 
 # %%
